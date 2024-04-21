4e9a47901d453213e34afdda333e1be50887e581
David <davideduardolino@outlook.com>
2024-04-21 16:41:42 -0300

    pagina de contato

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [LoadingComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  public loading: boolean = true; // Estado de carregamento
+  private date = new Date()
+  private mes = this.date.getMonth() - 1
+  private idade = this.date.getFullYear() - 2005
+  public age: any;
+  ngOnInit(): void {
+    if (this.mes <= 11) {
+      this.age = this.idade - 1
+    }
+    else {
+      this.age = this.idade
 
+    }
+
+    window.onload = () => {
+      this.loading = false; // Altera o estado de carregamento quando todos os recursos da página forem carregados
+    };
+
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private date = new Date()
+  private mes = this.date.getMonth() - 1
+  private idade = this.date.getFullYear() - 2005
+  public age: any;
+  ngOnInit(): void {
+    if (this.mes <= 11) {
+      this.age = this.idade - 1
+    }
+    else {
+      this.age = this.idade
+
+    }
+  }
 
 }
