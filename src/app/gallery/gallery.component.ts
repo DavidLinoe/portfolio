65b4201a8ba583fd285ec4022b053e1fe9a83aaf
David <davideduardolino@outlook.com>
2024-04-21 15:34:11 -0300

    refactoring

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [LoadingComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss'
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit{
+  loading: boolean = true; // Estado de carregamento
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Simulação de carregamento de imagens
+    setTimeout(() => {
+      this.loading = false; // Altera o estado de carregamento quando as imagens são carregadas
+    }, 2000); // Tempo de simulação, ajuste conforme necessário
+  }
 }
