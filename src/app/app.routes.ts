d7fa4e69ff67b5ac4eed4cc6b8fa90b17391b86d
David <davideduardolino@outlook.com>
2024-04-21 13:27:50 -0300

    estruturacao do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'gallery', component: GalleryComponent },
+    
+];
